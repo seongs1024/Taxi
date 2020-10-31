20685744a50c0bc48e57e95b733331bf87118332
seongs1024 <seongs1024@gmail.com>
2020-10-31 23:18:58 +0900

    add rendering method

--- src/environment.rs
+++ src/environment.rs
@@ -1,16 +1,42 @@
 use std::fmt;
 
-const DEFAULT_SIZE: i32 = 5;
+const DEFAULT_SIZE: usize = 5;
 
+#[derive(Clone)]
+pub enum Tile {
+    R,
+    G,
+    B,
+    Y,
+}
+
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", match self {
+            Self::R => "R",
+            Self::G => "G",
+            Self::B => "B",
+            Self::Y => "Y",
+        })
+    }
+}
 pub struct Env {
-    pub width: i32,
-    pub height: i32,
+    pub width: usize,
+    pub height: usize,
     finished: bool,
-    //q_table
+    tiles: Vec<Vec<Option<Tile>>>,
 }
 
 impl fmt::Display for Env {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.tiles.iter()
+            .for_each(|v| {
+                v.iter().for_each(|e| match e {
+                    Some(e) => print!("{}", e),
+                    None => print!("-"),
+                });
+                println!();
+            });
         write!(f, "WIDTH: {}, HEIGHT: {}", self.width, self.height)
     }
 }
@@ -22,11 +48,12 @@ impl Default for Env {
 }
 
 impl Env {
-    pub fn new(width: i32, height: i32) -> Self {
+    pub fn new(width: usize, height: usize) -> Self {
         Self {
             width,
             height,
             finished: false,
+            tiles: vec![vec![None; width]; height],
         }
     }
 }
